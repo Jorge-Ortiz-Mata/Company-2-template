47d53f600eb800f2f5695282597a3060563f582a
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-30 20:28:40 -0600

    Adding content to other pages.

--- javascript/privacy.js
+++ javascript/privacy.js
@@ -1,5 +1,4 @@
 const nav_bar_menu = document.querySelector('.nav_bar_menu');
-const seeMoreButton = document.querySelector('#seeMoreButton');
 const company_about = document.querySelector('.company_about');
 
 scrollFunction = () => {
@@ -12,14 +11,4 @@ scrollFunction = () => {
     }
 }
 
-seeMoreFunction = () =>{
-    console.log(company_about.offsetTop);
-    console.log(nav_bar_menu.pageYOffset);
-    window.scrollTo({
-        left: 0,
-        top: company_about.offsetTop - 80
-    });
-}
-
 window.addEventListener('scroll', scrollFunction);
-seeMoreButton.addEventListener('click', seeMoreFunction);
